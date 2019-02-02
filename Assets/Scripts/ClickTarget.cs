8b279c8df9c95f5430c62a4a5be787cfe47bc782
Fynn杨奉庆Fynn杨奉庆 <yfqing0302@163.com>
2019-02-02 14:11:44 +0800

    first commit

---
+++ Assets/Scripts/ClickTarget.cs
@@ -0,0 +1,31 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClickTarget : NetworkBehaviour, IInputClickHandler {
+    [SyncVar] private Color color;
+    private Material mat;
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        
+    }
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        ChangeColor();
+    }
+
+    private void Update()
+    {
+        mat.color = color;
+    }
+
+    private void ChangeColor()
+    {
+        color = UnityEngine.Random.ColorHSV();
+        mat.color = color;
+    }
+
+}
